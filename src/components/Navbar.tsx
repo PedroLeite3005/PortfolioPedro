61fcb74f823e85be50b1e3c62439379364dc0805
Pedro Bastos Leite <pedrobastos747@gmail.com>
2025-12-24 11:35:33 -0300

    cores navbar corrigida

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "./theme/ThemeToggle";
 
 export function Navbar() {
 	return (
-		<nav className="w-full border-b relative z-10 dark:bg-slate-950">
+		<nav className="w-full border-b-2 relative z-10 dark:bg-slate-950">
 			<div className="container mx-auto px-4 py-4">
 				<div className="flex items-center justify-between">
 					<a href="/" className="text-xl font-bold">
