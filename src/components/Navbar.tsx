f9a4b270daeb5a2f6fd20ba461b8c076338c3a89
Pedro Bastos Leite <pedrobastos747@gmail.com>
2025-12-29 16:56:27 -0300

    IntroAboutSkills
    - Intro e About finalizados
    - Corrigir Skills

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "./theme/ThemeToggle";
 
 export function Navbar() {
 	return (
-		<nav className="w-full border-b-2 relative z-10 dark:bg-slate-950">
+		<nav className="w-full border-b-2 relative z-10 dark:bg-slate-950 sticky top-0">
 			<div className="container mx-auto px-4 py-4">
 				<div className="flex items-center justify-between">
 					<a href="/" className="text-xl font-bold">
@@ -24,4 +24,3 @@ export function Navbar() {
 }
 
 export default Navbar;
-
